11e1ded9753a445f18882048ad1b20acc6d38f57
Muakhir <almuakhirjones@gmail.com>
2024-03-08 09:32:01 +0200

    ENV/Config

---
+++ config/index.js
@@ -0,0 +1,10 @@
+let sqlConnect = createPool({
+    host:process.env.DB_HOST,
+    database:process.env.DB_Name,
+    user:process.env.DB_UserName,
+    password:process.env.DB_UserPass,
+    multipleStatements: true,
+    connectionLimit: 30
+})
+
+export{sqlConnect}
