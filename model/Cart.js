975d44ea6b0340719c373a85298712da50ea1fc9
Muakhir <almuakhirjones@gmail.com>
2024-03-11 10:56:33 +0200

    Cart/update

--- model/Cart.js
+++ model/Cart.js
@@ -31,7 +31,7 @@ class Cart {
     async updateCartItemQuantity(req, res) {
         const { cartItemId } = req.params;
         const { prodQuantity } = req.body;
-        const qry = `UPDATE Cart SET quantity = ? WHERE id = ?`;
+        const qry = `UPDATE Cart SET quantity = ? WHERE cartItemId = ?`;
         db.query(qry, [prodQuantity, cartItemId], (err) => {
             if (err) throw err;
             res.json({ status: res.statusCode, message: 'Cart item quantity updated' });
