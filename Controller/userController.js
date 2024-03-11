64ebe9751c95551d32bd6ac363ffbeaf8e7be6ef
Muakhir <almuakhirjones@gmail.com>
2024-03-11 13:36:47 +0200

    Update/Cart

--- Controller/userController.js
+++ Controller/userController.js
@@ -66,4 +66,39 @@ res.json({status: res.statusCode, msg: "Failed to login"})
 }
 })
 
+userRouter.get('users/:id/cart', (req, res) => {
+    try {
+        users.fetchCartItems(req, res);
+    } catch (e) {
+        res.json({ status: res.statusCode, msg: 'Failed to retrieve cart items' });
+    }
+});
+
+// Add to Cart
+userRouter.post('users/:id/add', (req, res) => {
+    try {
+        users.addToCart(req, res);
+    } catch (e) {
+        res.json({ status: res.statusCode, msg: 'Failed to add item to cart' });
+    }
+});
+
+// Remove Item
+userRouter.delete('users/:id/:cartItemId/delete', (req, res) => {
+    try {
+        users.removeFromCart(req, res);
+    } catch (e) {
+        res.json({ status: res.statusCode, msg: 'Failed to remove item from cart' });
+    }
+});
+
+// Update Item Quantity
+userRouter.patch('users/:id/:cartItemId/update', (req, res) => {
+    try {
+        users.updateCartItemQuantity(req, res);
+    } catch (e) {
+        res.json({ status: res.statusCode, msg: 'Failed to update cart item quantity' });
+    }
+});
+
 export{userRouter,express}
