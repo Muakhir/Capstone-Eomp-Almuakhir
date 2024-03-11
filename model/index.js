5092678c24aebd6cc08dde989a71b0c55ee7fec6
Muakhir <almuakhirjones@gmail.com>
2024-03-11 13:44:34 +0200

    Update/Cart

--- model/index.js
+++ model/index.js
@@ -1,9 +1,9 @@
 import { Users } from "./Users.js";
 import { Products } from "./Products.js";
-import { Cart } from "./Cart.js";
+// import { Cart } from "./Cart.js";
 
 let users = new Users();
 let products = new Products();
 // let cart = new Cart();
 
-export{users, products, cart};
+export{users, products};
